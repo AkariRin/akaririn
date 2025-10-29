38276316503eb099bd40228a2aec9aeab26d9c06
梦清 <akaririn1028@gmail.com>
2025-10-29 21:20:49 +0800

    Add Vuetify and font support with Roboto to the project

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'unfonts.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(createVuetify({
+    components,
+    directives,
+  }))
+  .mount('#app')
